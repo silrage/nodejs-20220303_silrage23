59993c54fa9d90c034bd358f08e38ed2fbc8befc
Viktor.Sidorets <viktor.sidorets@south.rt.ru>
2022-03-20 23:14:56 +0300

    solution for 3.2 task

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,32 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.rowContext = '';
+    this.separator = options.separator || os.EOL;
   }
 
-  _transform(chunk, encoding, callback) {
+  _transform(chunk, encoding, cb) {
+    Array.from(chunk.toString()).forEach((symbol) => {
+      if (symbol.includes(this.separator)) {
+        // Sepearated rows
+        this.push(this.rowContext);
+        this.rowContext = '';
+      } else {
+        // Concatenated symbols in row
+        this.rowContext += symbol;
+      }
+    });
+    cb();
   }
 
-  _flush(callback) {
+  _flush(cb) {
+    if (this.rowContext) {
+      // For last row
+      this.push(this.rowContext);
+      this.rowContext = '';
+    }
+    cb();
   }
 }
 
