4d1f606abce8143104a1fbfba42a256aea93dfdc
Viktor.Sidorets <viktor.sidorets@south.rt.ru>
2022-04-09 01:51:08 +0300

    add 6 module 3 task

--- 06-mongodb-mongoose/03-search/controllers/products.js
+++ 06-mongodb-mongoose/03-search/controllers/products.js
@@ -1,3 +1,12 @@
+const modelProduct = require('../models/Product.js');
+const mapperProduct = require('../mappers/product.js');
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+  const {query} = ctx.query;
+  const products = await modelProduct.find(
+    {$text: {$search: query}},
+    {score: {$meta: 'textScore'}},
+  ).sort({score: {$meta: 'textScore'}});
+  
+  ctx.body = {products: products.map(mapperProduct)};
 };
