25a1d2559c596a3a8c88b9877771f223a36a79bc
Viktor.Sidorets <viktor.sidorets@south.rt.ru>
2022-04-12 00:43:29 +0300

    add module 7 task 1

--- 07-authentication/01-local/libs/strategies/local.js
+++ 07-authentication/01-local/libs/strategies/local.js
@@ -1,8 +1,18 @@
 const LocalStrategy = require('passport-local').Strategy;
+const User = require('../../models/User.js');
 
 module.exports = new LocalStrategy(
     {usernameField: 'email', session: false},
-    function(email, password, done) {
-      done(null, false, 'Стратегия подключена, но еще не настроена');
+    async function(email, password, done) {
+      User.findOne({email}, async function(err, user) {
+        if (err) return done(err);
+        if (!user) return done(null, false, 'Нет такого пользователя'); // DON'T USE IN PROD
+
+        const isVerified = await user.checkPassword(password);
+        return isVerified
+          ? done(null, user)
+          : done(null, false, 'Неверный пароль')
+        ;
+      });
     },
 );
