8200eaacd8e34fa2c002a694eb3524b69d574e5e
Viktor.Sidorets <viktor.sidorets@south.rt.ru>
2022-04-28 00:45:08 +0300

    add solution for 7-mod, 1 task
    fix config 6 mod

--- 07-authentication/02-oauth/libs/strategies/authenticate.js
+++ 07-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,3 +1,18 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const User = require('../../models/User.js');
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  if (!email) {
+    return done(null, false, 'Не указан email');
+  }
+
+  try {
+    let user = await User.findOne({email});
+    if (!user) {
+      // Create new user
+      user = await User.create({email, displayName});
+    }
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
 };
